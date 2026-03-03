8233e0c92d69f1bb23931e2c48e217c93a36cf96
SOUFIYAN IGUIDER <office.contact.sufian@gmail.com>
2026-03-03 23:31:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-212586660-8f26d",
+  "appId": "1:754311533284:web:eb54e613a97e21741d92f1",
+  "apiKey": "AIzaSyCL5QM2mnHXkd4YGajuKvtDys8CWyufDc0",
+  "authDomain": "studio-212586660-8f26d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "754311533284"
+};
