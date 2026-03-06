a2ff937dcbd29759959a01fe47a89e0b7857b634
SOUFIYAN IGUIDER <office.contact.sufian@gmail.com>
2026-03-06 16:11:02 +0000

    ## Error Type Runtime ReferenceError
    ## Error Message
    CardDescription i

--- src/app/crop/page.tsx
+++ src/app/crop/page.tsx
@@ -21,7 +21,7 @@ import {
   RefreshCcw
 } from 'lucide-react';
 import { useToast } from '@/hooks/use-toast';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { PDFDocument } from 'pdf-lib';
