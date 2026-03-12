9e3f04bf1b5e734a2cde185f4f4843d59e5b9f01
SOUFIYAN IGUIDER <office.contact.sufian@gmail.com>
2026-03-12 23:54:23 +0000

    ## Error Type Build Error
    ## Error Message
    Module not found: Can't reso

--- src/app/[locale]/signup/page.tsx
+++ src/app/[locale]/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import * as React from 'export react';
+import * as React from 'react';
 import { useRouter, useParams } from 'next/navigation';
 import { Navbar } from '@/components/navbar';
 import { Button } from '@/components/ui/button';
